481c0bdd3469602390a5d8e2849e2c1dda090797
tigreclip <111276784+tigreclip@users.noreply.github.com>
2023-01-22 19:58:01 +0100

    descargar texto de una web y eleminar espacios

---
+++ scrap_BeutifulSoup.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+import requests
+
+website = 'https://subslikescript.com/movie/Titanic-120338'
+
+# envio de solicitud a la web
+respuesta = requests.get(website)
+
+# obtencion del texto
+contenido = respuesta.text
+
+# localizar elementos en una web,
+sopa = BeautifulSoup(contenido, 'lxml')
+
+# ver resultados con formato html
+# print(sopa.prettify())
+
+# orden de búsqueda:
+# 1.id
+# 2.Class name 3
+# 3.Tag name, CSS selector
+# 4. Xpath
+
+# contenido seleccionado usando id, class
+caja = sopa.find('article', class_ = 'plot')
+
+# nombre de la película
+titulo = sopa.find('h1').get_text()
+# strip=True -> elimina los espacios al principio y al final del string al poner como separador ' '
+transcripcion = sopa.find('div', class_ = "full-script").get_text(strip = True, separator = ' ')
+print(titulo)
+print(transcripcion)
